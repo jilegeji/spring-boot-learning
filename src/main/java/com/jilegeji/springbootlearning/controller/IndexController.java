0091c72125f8be27904861dabbc17207ad4dd941
yangyuzhe <663664775@qq.com>
2020-07-17 20:35:40 +0800

    代码更新

---
+++ src/main/java/com/jilegeji/springbootlearning/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.jilegeji.springbootlearning.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping("/index")
+	public String index(Model model){
+		return "index";
+	}
+}
